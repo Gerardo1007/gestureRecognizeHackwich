601f3dfe5176c0dafa34c2c4a46c1e2428e17dc3
GERARDO DEHUMA <rl200066@rlas-macair-23.local>
2018-11-08 10:28:35 -0600

    Initial Commit

---
+++ gestureRecognizeHackwich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  gestureRecognizeHackwich
+//
+//  Created by GERARDO DEHUMA on 11/8/18.
+//  Copyright © 2018 GERARDO DEHUMA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
